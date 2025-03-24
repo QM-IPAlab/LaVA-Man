a0ad4b233157811541362ce988b1b4d44ddca57d
Noone65536 <edward.wdz@outlook.com>
2025-03-24 14:06:02 +0000

    debug for scripts on sulis

--- scripts_sulis/debug_pretrain.sh
+++ scripts_sulis/debug_pretrain.sh
@@ -8,7 +8,7 @@ export MASTER_ADDR=$(hostname)
 export MASTER_PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 
 python mae/main_pretrain_ours.py \
-    --model mae_fuse \
+    --model mae_fuse_tt \
     --batch_size 96 \
     --input_size 224 224  \
     --output_dir  debug \
