b888b4307ae3a88f72677181b07fbb4aed316330
Noone65536 <edward.wdz@outlook.com>
2025-04-12 22:47:46 +0100

    split seen and unseen tasks script

--- scripts/batch_train_sep_multi.sh
+++ scripts/batch_train_sep_multi.sh
@@ -25,54 +25,62 @@ export TOKENIZERS_PARALLELISM=false
 # 8. check the agent name: sep or not sept, if sep, check train.sep_mode is set to pick or place
 
 
-exps_name="exps/bridge_droid_2"
+exps_name="exps/multisize_new_split2"
 agent_name="mae_sep_seg2_add"
-pretrain_path="/home/robot/Repositories_chaoran/MPI/checkpoints/ours/checkpoint-399-droid.pth"
+pretrain_path="/home/robot/Repositories_chaoran/CLIPort_new_loss/exps/checkpoint-100-multisize.pth"
 mae_model="mae_robot_lang"
 #pretrain_path=False
 
-# tasks for ablation study (mask ratio)
-# tasks=("assembling-kits-seq-seen-colors"
-#   "towers-of-hanoi-seq-seen-colors"
-#   "stack-block-pyramid-seq-seen-colors"
-#   "separating-piles-seen-colors"
-#   "put-block-in-bowl-seen-colors"
-#   "packing-boxes-pairs-seen-colors"
-#   "packing-seen-google-objects-group"
-#   "packing-seen-google-objects-seq"
-#   "packing-unseen-google-objects-group"
-#   "packing-unseen-google-objects-seq"
-# )
+#tasks for ablation study (mask ratio)
+tasks=("assembling-kits-seq-seen-colors"
+  "assembling-kits-seq-unseen-colors"
+  "towers-of-hanoi-seq-seen-colors"
+  "towers-of-hanoi-seq-unseen-colors"
+  "stack-block-pyramid-seq-seen-colors"
+  "stack-block-pyramid-seq-unseen-colors"
+  "separating-piles-seen-colors"
+  "separating-piles-unseen-colors"
+  "put-block-in-bowl-seen-colors"
+  "put-block-in-bowl-unseen-colors"
+  "packing-boxes-pairs-seen-colors"
+  "packing-boxes-pairs-unseen-colors"
+  "packing-seen-google-objects-group"
+  "packing-seen-google-objects-seq"
+  "packing-unseen-google-objects-group"
+  "packing-unseen-google-objects-seq"
+  "align-rope"
+  "packing-shapes"
+)
 
 #tasks for testing
-tasks=("assembling-kits-seq-full"\
-    "packing-boxes-pairs-full"\
-    "stack-block-pyramid-seq-full"\
-    "towers-of-hanoi-seq-full"\
-    "put-block-in-bowl-full"\
-    "packing-seen-google-objects-group"\
-    "packing-unseen-google-objects-group"\
-    "packing-seen-google-objects-seq"\
-    "packing-unseen-google-objects-seq"\
-    "separating-piles-full"\
-    #"align-rope"\
-    #"packing-shapes"\
-)
+# tasks=("assembling-kits-seq-full"\
+#     "packing-boxes-pairs-full"\
+#     "stack-block-pyramid-seq-full"\
+#     "towers-of-hanoi-seq-full"\
+#     "put-block-in-bowl-full"\
+#     "packing-seen-google-objects-group"\
+#     "packing-unseen-google-objects-group"\
+#     "packing-seen-google-objects-seq"\
+#     "packing-unseen-google-objects-seq"\
+#     "separating-piles-full"\
+#     "align-rope"\
+#     "packing-shapes"\
+# )
 
 python -m cliport.train  train.task=multi-language-conditioned\
                          train.agent=${agent_name}\
                          train.exp_folder=${exps_name}\
                          wandb.run_name=${exps_name}_multi\
-                         train.n_demos=1000 \
+                         train.n_demos=100 \
                          train.n_steps=60100 \
                          train.lr_scheduler=True\
                          train.lr=2e-5\
-                         train.warmup_epochs=10\
+                         train.warmup_epochs=20\
                          train.precision=32\
                          train.batch_size=32\
                          train.batchnorm=True\
                          train.load_from_last_ckpt=False\
-                         train.log=True\
+                         train.log=False\
                          mae_model=${mae_model} \
                          pretrain_path=${pretrain_path}\
                          cliport_checkpoint=False\
@@ -86,16 +94,16 @@ python -m cliport.train  train.task=multi-language-conditioned\
                          train.agent=${agent_name}\
                          train.exp_folder=${exps_name}\
                          wandb.run_name=${exps_name}_multi\
-                         train.n_demos=1000 \
+                         train.n_demos=100 \
                          train.n_steps=60100 \
                          train.lr_scheduler=True\
                          train.lr=2e-5\
-                         train.warmup_epochs=10\
+                         train.warmup_epochs=20\
                          train.precision=32\
                          train.batch_size=8\
                          train.batchnorm=True\
                          train.load_from_last_ckpt=False\
-                         train.log=True\
+                         train.log=False\
                          mae_model=${mae_model} \
                          pretrain_path=${pretrain_path}\
                          cliport_checkpoint=False\
@@ -139,10 +147,10 @@ do
                         eval_task=${task} \
                         agent=${agent_name} \
                         mode=test \
-                        n_demos=100 \
-                        train_demos=1000 \
+                        n_demos=50 \
+                        train_demos=100 \
                         exp_folder=${exps_name} \
-                        checkpoint_type=last \
+                        checkpoint_type=best \
                         update_results=True \
                         disp=False\
                         record.save_video=False
